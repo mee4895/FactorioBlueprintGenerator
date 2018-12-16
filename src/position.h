dc15e3ae823fec1d129d1f5f161936f9f8ca0a56
Tobias Wältken <tobias.w@ltken.de>
2018-12-16 04:35:41 +0100

    Fix some more dumb bugs

--- src/position.h
+++ src/position.h
@@ -8,70 +8,70 @@ class Position
 public:
 	Position()
 	{
-		this.x = 0;
-		this.y = 0;
+		this->x = 0;
+		this->y = 0;
 	}
 	Position(int x, int y)
 	{
-		this.x = x;
-		this.y = y;
+		this->x = x;
+		this->y = y;
 	}
 	Position(float x, float y)
 	{
-		this.x = x;
-		this.y = y;
+		this->x = x;
+		this->y = y;
 	}
 
 	inline float getX()
 	{
-		return this.x;
+		return this->x;
 	}
 	inline float getY()
 	{
-		return this.y;
+		return this->y;
 	}
 
 	inline void setX(float x)
 	{
-		this.x = x;
+		this->x = x;
 	}
 	inline void setY(float y)
 	{
-		this.y = y;
+		this->y = y;
 	}
 	inline void setX(int x)
 	{
-		this.x = x;
+		this->x = x;
 	}
 	inline void setY(int y)
 	{
-		this.y = y;
+		this->y = y;
 	}
 
-	inline void add(Position p)
+	inline void add(Position& p)
 	{
-		this.x += p.x;
-		this.y += p.y;
+		this->x += p.x;
+		this->y += p.y;
 	}
-	inline void sub(Position p)
+	inline void sub(Position& p)
 	{
-		this.x -= p.x;
-		this.y -= p.y;
+		this->x -= p.x;
+		this->y -= p.y;
 	}
 
-	inline bool getJsonString(std::stringstream stream)
+	inline bool getJsonString(std::stringstream& stream)
 	{
 		if (!stream.good()) return false;
 
-		stream << "{\"x\": " << this.x << ",\"y\": " << this.y << "}";
+		stream << "{\"x\":" << this->x << ",\"y\":" << this->y << "}";
 		return true;
 	}
 
-	static inline Position add(Position a, Position b)
+	static inline Position add(Position& a, Position& b)
 	{
 		return Position(a.x + b.x, a.y + b.y);
 	}
-	static inline Position sub(Position a, Position b)
+	static inline Position sub(Position& a, Position& b)
 	{
 		return Position(a.x - b.x, a.y - b.y);
 	}
