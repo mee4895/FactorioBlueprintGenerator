1c8552679d8ff28ebb8886c34101dee7463a718a
Tobias Wältken <tobias.w@ltken.de>
2018-12-16 04:36:49 +0100

    Add the Tile class

---
+++ src/factorio/tile.h
@@ -0,0 +1,67 @@
+#ifndef TILE_H
+#define TILE_H
+
+#include <string>
+#include <sstream>
+#include "position.h"
+
+namespace factorio
+{
+	class Tile
+	{
+	public:
+		enum Level
+		{
+			NONE,
+			STONE_BRICK,
+			CONCRETE,
+			HAZARD_CONCRETE,
+			REFINED_CONCRETE,
+			REFINED_HAZARD_CONCRETE,
+
+			NUM_LEVELS
+		};
+
+		Tile(Level level, Position position)
+		{
+			this->level = level;
+			this->position = position;
+		}
+
+		inline bool getJsonString(std::stringstream& stream)
+		{
+			if (this->level == Level::NONE) return false;
+			if (!stream.good()) return false;
+
+			stream << "{\"name\":\"" << getLevelName(this->level) << "\",\"position\":";
+			this->position.getJsonString(stream);
+			stream << "}";
+			return true;
+		}
+
+	protected:
+		inline std::string getLevelName(Level level) {
+			switch (level)
+			{
+				case STONE_BRICK:
+					return "stone-brick";
+				case CONCRETE:
+					return "concrete";
+				case HAZARD_CONCRETE:
+					return "hazard-concrete";
+				case REFINED_CONCRETE:
+					return "refined-concrete";
+				case REFINED_HAZARD_CONCRETE:
+					return "refined-hazard-concrete";
+				default:
+					return "";
+			}
+		}
+
+	private:
+		Level level;
+		Position position;
+	};
+}
+
+#endif // TILE_H
